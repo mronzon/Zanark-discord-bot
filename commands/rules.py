7edb28eb0857e3119e75a99ee29db10ee34781a3
mronzon <104356392+mronzon@users.noreply.github.com>
2024-01-12 01:02:59 +0100

    Ajout de l'envoie de message dans le salon regles

--- commands/rules.py
+++ commands/rules.py
@@ -1,15 +1,56 @@
 from discord import app_commands
 import discord
-
+import json
 
 class Rules(app_commands.Group):
     """Manage the command related to the rules"""
-    def __init__(self, client: discord.Client):
+    def __init__(self, client: discord.Client, env_path: str):
         super().__init__()
         self.bot = client
+        self.channel = None
+        self.env_path = env_path
+        self.env = None
+        self.wait_message = False
+        self.channel_wait = None
+        with open(self.env_path, 'r', encoding='UTF-8') as file:
+            self.env = json.load(file)
+        
 
-    @app_commands.command(name="setchannel", description="Permet d'indiquer le channel dans lequel les règles sont.")
-    async def essai(self, interaction: discord.Interaction):
-        print()
-        await interaction.response.send_message("Hello !")
+    @app_commands.command(name="salon", description="Permet d'indiquer le salon textuel dans lequel les règles sont.")
+    async def salon(self, interaction: discord.Interaction, channel: str):  
+        try:
+            channel_id = channel.split("#")[1][:-1]
+            channel_id = int(channel_id)
+            channel = self.bot.get_channel(channel_id)
+            await interaction.response.send_message("Le channel a été mis à jour")
+            self.channel = channel
+            self.env["rules_channel"] = channel_id
+            self._save_env()
+        except:
+            await interaction.response.send_message("Veuillez me donner un salon textuel seulement.")
+    
+    @app_commands.command(name="text", description="Permet de mettre un texte dans le channel règles.")
+    async def text(self, interaction: discord.Interaction):
+        if self.channel == None:
+            await interaction.response.send_message("Aucun salon textuel a été choisi !")
+            return
+        await interaction.response.send_message("Veuillez donner le texte :")
+        self.wait_message = True
+        self.channel_wait = interaction.channel
+
+    async def message_rules(self, message: discord.Message):
+        await self.channel.send(message.content)
+        self.wait_message = False
+        self.channel_wait = None
+
+    def _get_env(self):
+        if self.env["rules_channel"] != 0:
+            self.channel = self.bot.get_channel(self.env["rules_channel"])
+    
+    def _save_env(self):
+        with open(self.env_path, 'w', encoding='UTF-8') as file:
+            file.write(json.dumps(self.env, indent=4, ensure_ascii=False))
+    
+
+        
 
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD")
 GUILD = int(os.getenv("GUILD"))
+env_path = os.getenv("ENV_PATH")
 guild_object = discord.Object(id=GUILD)
 
 
@@ -23,17 +24,26 @@ class aclient(discord.Client):
             await tree.sync(guild=guild_object)
             self.synced = True
         print(f"We have logged in as {self.user}.")
+        rules._get_env()
+    
+    async def on_message(self, message: discord.Message):
+        if message.author.id == self.user.id:
+            return
+        
+        if rules.wait_message and message.channel == rules.channel_wait:
+            await rules.message_rules(message)
+
 
 
 client = aclient()
 tree = app_commands.CommandTree(client)
-
+rules = Rules(client, env_path)
 
 @tree.command(name="test", description="testing", guild=guild_object)
 async def self(interaction: discord.Interaction, name: str):
     await interaction.response.send_message(f"Hello {name} !")
-    
-tree.add_command(Rules(client), guild=guild_object)
+
+tree.add_command(rules, guild=guild_object)
 
 client.run(TOKEN)
 
