4601909497e39f6e2c51ad6e44c51d507f9fa08a
mronzon <mathis.ronzon@gmail.com>
2025-08-21 21:31:16 +0200

    Fix bug flame 6

--- src/bdg_scrapper.py
+++ src/bdg_scrapper.py
@@ -36,18 +36,28 @@ def get_scores(image_path):
 
   results = []
   for i, (x, y, w, h) in enumerate(score_boxes):
-      # Extraction score
-      roi_score = gray[y:y+h, x:x+w]
+      # Extraction score avec marge réduite pour éviter les artefacts
+      margin = 5
+      roi_score = gray[y+margin:y+h-margin, x+margin:x+w-margin]
       roi_score = cv2.threshold(roi_score, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+      
       config_score = r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789'
       text_score = pytesseract.image_to_string(roi_score, config=config_score)
-      score = ''.join(filter(str.isdigit, text_score))
+      
+      # Nettoyage du score : si il y a un espace, prendre seulement la partie après l'espace
+      if ' ' in text_score:
+          score_raw = text_score.split(' ')[-1]  # Prendre la dernière partie après l'espace
+      else:
+          score_raw = text_score
+      
+      score = ''.join(filter(str.isdigit, score_raw))
 
       # Extraction nom à gauche du score
       name_x1 = max(0, x - 1000)
       name_x2 = x - 10
       roi_name = gray[y:y+h, name_x1:name_x2]
       roi_name = cv2.threshold(roi_name, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+      
       config_name = r'--oem 3 --psm 7'
       text_name = pytesseract.image_to_string(roi_name, config=config_name)
       # Nettoyage du nom : extraire le premier mot de 3+ lettres sans caractères spéciaux
